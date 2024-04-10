3e0ea3f5e1d82b88b8ee1c925470540d4f2d9957
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-04-10 20:04:41 -0300

    updt

--- pages/admin/clientes/index.vue
+++ pages/admin/clientes/index.vue
@@ -92,34 +92,38 @@ function formatarData(input) {
     input.value = `${dia}-${mes}-${ano}`;
 }
 
-</script>
-<template>
-    <div v-if="add" class="main">
 
-        <div class="nav-top">
-            <div class="clients">
-                <Icon name='solar:users-group-two-rounded-bold' /> Clientes <span> {{ Users.data.value.length }} </span>
-            </div>
-            <div>
-                <div class="add-client" @click="addClient">Adicionar Cliente
+</script>
+
+<template>
+
+    <div v-if="add" id="grid">
+        <div  id="areaA">
+            <div class="nav-top">
+                <div class="clients">
+                    <Icon name='solar:users-group-two-rounded-bold' /> Clientes <span> {{ Users.data.value.length }} </span>
+                </div>
+                <div>
+                    <div class="add-client" @click="addClient">Adicionar Cliente
                     <Icon name='material-symbols:add' />
                 </div>
-            </div>
-        </div>
-        <div class="nav-users">
-            <div class="users-conf">
-                <!-- Clientes <span> {{ Users.data.value.length  }} </span> -->
-            </div>
-            <div>
-                <div v-if="swit" class="filter" @click="switchButton">
-                    <Icon name='material-symbols:view-list-outline' /> Lista
-                </div>
-                <div v-else class="filter" @click="switchButton">
-                    <Icon name='solar:widget-linear' /> Blocos
                 </div>
             </div>
-        </div>
+            <div class="nav-users">
+                <div class="users-conf">
+                    <!-- Clientes <span> {{ Users.data.value.length  }} </span> -->
+                </div>
+                <div>
+                    <div v-if="swit" class="filter" @click="switchButton">
+                        <Icon name='material-symbols:view-list-outline' /> Lista
+                    </div>
+                    <div v-else class="filter" @click="switchButton">
+                        <Icon name='solar:widget-linear' /> Blocos
+                    </div>
+                </div>
+            </div>
 
+        </div>
         <div>
             <div v-if="block" class="users-list">
                 <div v-for="(item, index) in item" :key="index">
@@ -224,7 +228,6 @@ function formatarData(input) {
             </div>
         </div>
     </div>
-
     <div v-else class="main">
         <div class="barTop center">
             <div>
@@ -297,9 +300,184 @@ function formatarData(input) {
         </div>
     </div>
 </template>
-
 <style scoped>
+.Doughnut {
+    width: 320px;
+    height: 320px;
+}
+.nav-top {
+    position: sticky;
+    top: 0px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    width: 100%;
+    z-index: 1;
+    height: 40px;
+    font-weight: bolder;
+    border-bottom: .10px solid #34d39940;
+    backdrop-filter: blur(45px);
+    border-bottom: solid 1px #34d39940;
+    border-right: solid 1px #34d39940;
+}
 
+.clients {
+    margin: 11px;
+}
+
+.clients span {
+    border: 1px solid #34d39990;
+    padding: 3px 6px;
+    border-radius: 5px;
+    color: #34d399;
+    background-color: #34d39930;
+    margin-left: 3px;
+}
+
+.add-client {
+    border: solid 1px #04be7a90;
+    background-color: #04be7a;
+    padding: 6px 7px;
+    margin: 4px;
+    border-radius: 5px;
+    transition: all .5s linear;
+    cursor: pointer;
+    color: #fff;
+}
+
+.add-client:hover {
+    border: solid 1px #fff;
+    padding: 6px 7px;
+    border-radius: 8px;
+    color: #04be7a;
+    background-color: #fff;
+}
+
+.notifications {
+    border: solid 1px transparent;
+    padding: 4px 5px;
+    margin: 6px;
+    border-radius: 5px;
+    transition: all .5s linear;
+    cursor: pointer;
+}
+
+.notifications:hover {
+    padding: 4px 5px;
+    border-radius: 5px;
+    color: #34d399;
+    background-color: #fff;
+}
+
+.nav-users {
+    position: sticky;
+    top: 40px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    width: 100%;
+    z-index: 1;
+    height: 50px;
+    font-weight: bolder;
+    margin-bottom: 1rem;
+    border-bottom: .10px solid #34d39940;
+    backdrop-filter: blur(45px);
+    border-bottom: solid 1px #34d39940;
+        border-right: solid 1px #34d39940;
+}
+
+.users-conf {
+    margin: 16px;
+}
+
+.users-conf span {
+    border: 1px solid #34d39990;
+    padding: 8px;
+    border-radius: 5px;
+    color: #34d399;
+    background-color: #34d39930;
+    margin-left: 3px;
+}
+
+.filter {
+    border: solid 1px #34d39990;
+    background-color: #34d39930;
+    padding: 6px 7px;
+    margin: 9px 35px;
+    border-radius: 5px;
+    transition: all .5s linear;
+    cursor: pointer;
+}
+
+.filter:hover {
+    border: solid 1px #fff;
+    padding: 6px 7px;
+    border-radius: 5px;
+    color: #34d399;
+    background-color: #fff;
+}
+
+.users-list {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    z-index: 1;
+    margin-bottom: 1rem;
+    overflow-y: auto;
+    overflow-x: hidden;
+}
+
+.color {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+    position: fixed;
+    height: 35px;
+    width: 35px;
+    transition: all 0.2s ease-in-out 0s;
+    bottom: 6rem;
+    right: 1.5rem;
+    border-radius: 9px;
+    cursor: pointer;
+    z-index: 100;
+    border: solid 1px #34d39910;
+    box-shadow: 0 0px 5px #34d39940;
+    backdrop-filter: blur(100px)
+}
+
+.whats {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+    position: fixed;
+    height: 35px;
+    width: 35px;
+    transition: all 0.2s ease-in-out 0s;
+    bottom: 3.5rem;
+    right: 1.5rem;
+    border-radius: 9px;
+    cursor: pointer;
+    z-index: 100;
+    border: solid 1px #34d39910;
+    box-shadow: 0 0px 5px #34d39940;
+    backdrop-filter: blur(100px)
+}
+
+.whats .icon,
+.color .icon {
+    color: #34d39990;
+    zoom: 1;
+}
 .subscriberOk {
     position: fixed;
     top: 10px;
